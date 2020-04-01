c95dd353711dcfb0e04197e18c8d8141407bc601
MJMarijnissen <kubamarijnissen@gmail.com>
2020-04-01 17:16:54 +0200

    changed data to categorical

--- CNN.py
+++ CNN.py
@@ -14,5 +14,9 @@ from tensorflow.keras.layers import Conv2D, MaxPool2D, Dense, Flatten, Dropout
 from tensorflow.keras.utils import to_categorical
 from data_exploration import read_data
 
-#%%%
+#%%
 X_train, y_train, X_test, y_test, dict_signs = read_data()
+
+if y_train.ndim == 1: y_train = to_categorical(y_train)
+if y_test.ndim == 1: y_test = to_categorical(y_test)
+
